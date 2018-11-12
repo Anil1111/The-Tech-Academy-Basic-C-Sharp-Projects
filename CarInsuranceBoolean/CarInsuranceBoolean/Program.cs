093e8cdb893c9ad089915076b3f752fd6b51c86d
KoderBug <mshaffner3@hotmail.com>
2018-11-11 23:08:12 -0700

    Added WriteLine "Qualified?" statement

--- CarInsuranceBoolean/CarInsuranceBoolean/Program.cs
+++ CarInsuranceBoolean/CarInsuranceBoolean/Program.cs
@@ -13,7 +13,8 @@
         Console.WriteLine("How many speeding tickets do you have? ");
         string speedTix = Console.ReadLine();
 
-        bool isQualified = ((Convert.ToInt32(age)) > 15) && (DUI.ToUpper() == "NO") && (Convert.ToInt32(speedTix) <= 3);
+        bool isQualified = ((Convert.ToInt32(age)) > 15) && (DUI.ToUpper() == "NO" || DUI.ToUpper() == "FALSE") && (Convert.ToInt32(speedTix) <= 3);
+        Console.WriteLine("Qualified? ");
         Console.WriteLine(isQualified.ToString());
         Console.ReadLine();
     }
