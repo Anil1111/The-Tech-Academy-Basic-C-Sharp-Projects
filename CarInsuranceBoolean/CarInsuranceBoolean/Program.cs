e3b21fa57ea3593f2bd5c669ce4ebc6480c2c6a4
KoderBug <mshaffner3@hotmail.com>
2018-11-11 22:54:16 -0700

    Created Boolean Logic Car Insurance Approval Program

---
+++ CarInsuranceBoolean/CarInsuranceBoolean/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your age? ");
+        string age = Console.ReadLine();
+
+        Console.WriteLine("Have you ever had a DUI? ");
+        string DUI = Console.ReadLine();
+
+        Console.WriteLine("How many speeding tickets do you have? ");
+        string speedTix = Console.ReadLine();
+
+        bool isQualified = ((Convert.ToInt32(age)) > 15) && (DUI.ToUpper() == "NO") && (Convert.ToInt32(speedTix) <= 3);
+        Console.WriteLine(isQualified.ToString());
+        Console.ReadLine();
+    }
+    }
