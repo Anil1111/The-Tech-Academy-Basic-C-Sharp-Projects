94f7c50de0776dc21ad777d745ec11a371ef152a
KoderBug <mshaffner3@hotmail.com>
2018-11-14 16:05:39 -0700

    Second Math Class Parameter Exercise

---
+++ MathParameterExercise2/MathParameterExercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathParameterExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass mathCall1 = new MathClass();
+            int answer = mathCall1.EasyMath(12);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+            MathClass mathCall2 = new MathClass();
+            answer = mathCall2.EasyMath(83.65m);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+            MathClass mathCall3 = new MathClass();
+            answer = mathCall3.EasyMath("75");
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
