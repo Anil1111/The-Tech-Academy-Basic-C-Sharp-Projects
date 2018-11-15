7d6d799aee2f65bfa2ebda37ae6afe5f534d0467
KoderBug <mshaffner3@hotmail.com>
2018-11-15 16:38:26 -0700

    Added generic list to Employee class, instantiated objects, added data and printed to console.

--- EmployeesPgm2/EmployeesPgm2/Employee.cs
+++ EmployeesPgm2/EmployeesPgm2/Employee.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace EmployeesPgm2
 {
-    class Employee : Person, IQuittable
+    class Employee<T> : Person, IQuittable
     {
+        public List<T> things;
         public int id = 0;
-
+               
         public override void SayName()
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);
@@ -22,28 +23,28 @@ namespace EmployeesPgm2
             Console.ReadLine();
         }
 
-        public static bool operator== (Employee employee1, Employee employee2)
-        {
-            if (employee1.id == employee2.id)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        //public static bool operator== (Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.id == employee2.id)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
 
-        public static bool operator!= (Employee employee1, Employee employee2)
-        {
-            if (employee1.id != employee2.id)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        //public static bool operator!= (Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.id != employee2.id)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 }
--- EmployeesPgm2/EmployeesPgm2/Program.cs
+++ EmployeesPgm2/EmployeesPgm2/Program.cs
@@ -10,30 +10,60 @@ namespace EmployeesPgm2
     {
         static void Main(string[] args)      
         {
+            Employee<int> empInt = new Employee<int>();
+            empInt.things = new List<int>();
+            empInt.things.Add(101);
+            empInt.things.Add(102);
+            empInt.things.Add(103);
+            empInt.things.Add(104);
+            empInt.things.Add(105);
 
-            var empArray =  new Employee[5];
-            for (int i = 0; i < empArray.Length; i++)
-            {
-                empArray[i] = new Employee();
+            Employee<string> empString = new Employee<string>();
+            empString.things = new List<string>();
+            empString.things.Add("Accounting");
+            empString.things.Add("Human Resources");
+            empString.things.Add("Professional Services");
+            empString.things.Add("Advanced Solutions");
+            empString.things.Add("Engineering");
+
+
+            Console.WriteLine("Employee Integers: ");
+            foreach (int empI in empInt.things)
+            {               
+                Console.WriteLine(empI);              
             }
 
-            empArray[0].id = 101;
-            empArray[1].id = 102;
-            empArray[2].id = 103;
-            empArray[3].id = 101;
-            empArray[4].id = 104;
+            Console.WriteLine("Employee Integers: ");
+            foreach (string empStr in empString.things)
+            {
+                Console.WriteLine(empStr);
+            }
+            Console.ReadLine();
+
+
+            //var empArray =  new Employee[5];
+            //for (int i = 0; i < empArray.Length; i++)
+            //{
+            //    empArray[i] = new Employee();
+            //}
+
+            //empArray[0].id = 101;
+            //empArray[1].id = 102;
+            //empArray[2].id = 103;
+            //empArray[3].id = 101;
+            //empArray[4].id = 104;
 
             //Employee e1 = new Employee() { firstName = "Sample", lastName = "Student" };           
- 
-            for (int i=0; i< empArray.Length; i++) 
-            {
-                for (int j=i+1; j< empArray.Length;j++)
-                    if (empArray[i] == empArray[j])
-                    {
-                        Console.WriteLine("Duplicate employee id " + empArray[i].id);
-                        Console.ReadLine();
-                    }                 
-            }
+
+            //for (int i=0; i< empArray.Length; i++) 
+            //{
+            //    for (int j=i+1; j< empArray.Length;j++)
+            //        if (empArray[i] == empArray[j])
+            //        {
+            //            Console.WriteLine("Duplicate employee id " + empArray[i].id);
+            //            Console.ReadLine();
+            //        }                 
+            //}
 
             //Employee quittable = new Employee();
             //quittable.Quit();
