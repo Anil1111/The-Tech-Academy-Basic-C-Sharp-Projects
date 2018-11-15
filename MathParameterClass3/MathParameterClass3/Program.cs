0df2205a02d1159521bff14b7bcfdf354fa1ef05
KoderBug <mshaffner3@hotmail.com>
2018-11-14 17:05:41 -0700

    Third Math Class Parameter Exercise

---
+++ MathParameterClass3/MathParameterClass3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathParameterClass3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass mathClass = new MathClass();
+            Console.WriteLine("Please enter a number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number or enter to skip: ");
+            string secondNumber = Console.ReadLine();
+
+            bool n2 = string.IsNullOrEmpty(secondNumber);
+            int rootAnswer = 0;
+            if (!n2)
+            { 
+                int Number2 = Convert.ToInt32(secondNumber);
+                rootAnswer = mathClass.Rooted(firstNumber, Number2);
+            } else
+            {
+                rootAnswer = mathClass.Rooted(firstNumber);
+            }
+           
+            Console.WriteLine("MathClass answer: " + rootAnswer);
+            Console.ReadLine();
+        }
+    }
+}
