92c2582118d3df4486391ee373c0f418f2831e90
KoderBug <mshaffner3@hotmail.com>
2018-11-14 21:03:56 -0700

    Employees Pgm 2 with Abstract class

---
+++ EmployeesPgm2/EmployeesPgm2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesPgm2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            employee.SayName();
+        }
+    }
+}
